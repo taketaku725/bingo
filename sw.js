0273803daa442b97f5fa9aa3c308ecc2c70ed053
taketaku725 <115962176+taketaku725@users.noreply.github.com>
2026-03-10 19:23:55 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
-const CACHE="bingo-v1"
+const CACHE = "bingo-v1"
 
-const ASSETS=[
-"/",
-"/index.html",
-"/style.css",
-"/script.js"
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js"
 ]
 
-self.addEventListener("install",e=>{
-e.waitUntil(
-caches.open(CACHE).then(c=>c.addAll(ASSETS))
-)
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  )
 })
 
-self.addEventListener("fetch",e=>{
-e.respondWith(
-caches.match(e.request).then(r=>r||fetch(e.request))
-)
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  )
 })
